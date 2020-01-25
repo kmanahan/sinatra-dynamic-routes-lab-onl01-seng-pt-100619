9f13705d4d0030554ad1b3b5051fc20571c236d8
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-25 14:17:42 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,7 @@ class App < Sinatra::Base
   
     get '/say/:number/:phrase' do 
     @num = params[:number].to_i
-    @phrase = params[:pharse] 
+    @phrase = params[:phrase] 
     @num.times{"#{@phrase}"}
     end 
 end
