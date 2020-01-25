4017bd0a27e9ccb829257e465fe6c39200a8fcc7
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-25 15:32:05 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -34,7 +34,7 @@ class App < Sinatra::Base
       @number1 = params[:number1].to_i 
       @number2 = params[:number2].to_i 
       str.to_s = "#{@number1 + @number2}" || "#{@number1 - @number2}"
-      
+      str
     end 
     
 end
