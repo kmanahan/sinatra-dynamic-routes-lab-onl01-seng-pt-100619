2274070a5066181c68dc3d0fc129efde7072dd90
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-25 14:41:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -30,11 +30,10 @@ class App < Sinatra::Base
   end 
     
     get '/:operation/:number1/:number2' do 
-      str = ""
       @number1 = params[:number1].to_i 
       @numbers = params[:number2].to_i 
-      "#{str += @number1 + || - || ** ||  @number2}"
-      str
+      "#{@number1  @number2}"
+  
     end 
     
 end
