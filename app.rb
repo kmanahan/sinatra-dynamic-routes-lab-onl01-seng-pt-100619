dc94d34f4261daac0e5ec690ac65824edc64a9bf
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-25 14:22:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -16,7 +16,7 @@ class App < Sinatra::Base
       str = ""
     @num = params[:number].to_i
     @phrase = params[:phrase] 
-    @num.times{str +="#{@phrase}\n"}
+    @num.times{str +="#{@phrase}"}
     str
     end 
 end
