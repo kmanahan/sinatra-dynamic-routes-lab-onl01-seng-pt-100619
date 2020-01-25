cce184160e00d268589dc879e6ed4a847eada950
Kelly Manahan <kelly.a.manahan@gmail.com>
2020-01-25 14:08:40 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -2,5 +2,21 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   # Write your code here!
-
+  get '/reversename/:name' do 
+    @reverse = params[:name]
+    "#{@reverse.reverse}"
+  end
+  
+  get '/square/:number' do 
+    @squared = params[:number].to_i
+    "#{@squared ** 2}"
+  end 
+  
+    get '/say/:number/:phrase' do 
+    @num = params[:number].to_i
+    @phrs = params[:pharse] 
+    # 10.times { |i| puts "hello #{i}" }
+    
+    
+    end 
 end
